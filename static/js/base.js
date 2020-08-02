e646e6e1dc2597e0b1c01b0f6e43ee282295c75f
Asuna Masuda <asuna.fitz@gmail.com>
2020-08-02 23:52:16 +0000

    Add a code for responsive Navbar for scrolling to base.js

---
+++ static/js/base.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+  var navberExtension = $("#navber-extension");
+  $(window).scroll(function () {
+    if (
+      $(this).scrollTop() > 100 &&
+      navberExtension.hasClass("navber-extension")
+    ) {
+      navberExtension
+        .removeClass("navber-extension")
+        .addClass("fixed-top");
+    } else if (
+      $(this).scrollTop() <= 100 &&
+      navberExtension.hasClass("fixed-top")
+    ) {
+      navberExtension
+        .removeClass("fixed-top")
+        .addClass("navber-extension");
+    }
+  });
+});
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() <= 100) {
+    $("#logo").css("display", "block");
+  } else {
+    $("#logo").css("display", "none");
+  }
+});
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 100) {
+    $("#logo-small").css("display", "block");
+  } else {
+    $("#logo-small").css("display", "none");
+  }
+});
