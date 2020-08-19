3df9efa406951ce83256a0c014e3295d22cc5d28
Asuna Masuda <asuna.fitz@gmail.com>
2020-08-19 21:24:06 +0000

    Add models: Product, Image, Color, Flower to products/models.py

--- products/models.py
+++ products/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=30)
+    category = models.CharField(max_length=30)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    occasion = models.CharField(max_length=30)
+    description = models.TextField()
+    unit = models.CharField(max_length=30, null=True, blank=True)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Image(models.Model):
+    image = models.ImageField(null=True, blank=True)
+    URL = models.URLField(max_length=1024, null=True, blank=True)
+    product_id = models.ForeignKey('Product', null=True,
+                                   blank=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.name
+
+
+class Color(models.Model):
+    name = models.CharField(max_length=30)
+    image_id = models.ManyToManyField('Image')
+
+    def __str__(self):
+        return self.name
+
+
+class Flower(models.Model):
+    name = models.CharField(max_length=30)
+    product_id = models.ManyToManyField('Image')
+
+    def __str__(self):
+        return self.name
