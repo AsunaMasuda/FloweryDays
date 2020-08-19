10fc2216f8732e419c81548621cf9a5988f7d5c8
Asuna Masuda <asuna.fitz@gmail.com>
2020-08-19 21:50:49 +0000

    Add Product, Category, Image and  Flower models to admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Product, Category, Image, Flower
 
-# Register your models here.
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Image)
+admin.site.register(Flower)
