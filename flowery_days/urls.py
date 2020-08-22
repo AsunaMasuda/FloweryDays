39b23f753579c73bfc21d523a6b9bdcaf2657bba
Asuna Masuda <asuna.fitz@gmail.com>
2020-08-22 20:40:20 +0000

    Add urlpatterns for django-debug-toolbar to urls.py

--- flowery_days/urls.py
+++ flowery_days/urls.py
@@ -23,3 +23,7 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += path('__debug__/', include(debug_toolbar.urls)),
