625295f04caa1fe8059f05c2c300486e52f84dd4
Asuna Masuda <asuna.fitz@gmail.com>
2020-08-27 20:27:38 +0000

    Add a product search function in products/views.py

--- products/views.py
+++ products/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from itertools import chain
+from functools import reduce
+import operator
 
 from .models import Product, Image, Color, Flower
 
@@ -20,20 +22,26 @@ def all_products(request):
 
     if request.GET:
         if 'q' in request.GET:
-            query = request.GET['q']
-            colors = colors.filter(Q(name__icontains=query))
-            flowers = flowers.filter(Q(name__icontains=query))
-            products = products.filter(Q(name__icontains=query) | Q(description__icontains=query))
+            queries = request.GET['q'].split()
+            colors =  colors.filter(reduce(operator.__or__, [Q(name__icontains=query) for query in queries]))
+            flowers =  flowers.filter(reduce(operator.__or__, [Q(name__icontains=query) for query in queries]))
+            products =  products.filter(reduce(operator.__or__, [Q(name__icontains=query) | Q(description__icontains=query) for query in queries]))
 
+            # QuerySet with images of a searched color
+            color_filtered = colors.prefetch_related('image_id__product_id')
+
+            # Getting pk of product table from all the filters
             products_product_id = products.values_list('pk', flat=True)
-            colors_product_id = colors.prefetch_related('image_id').values_list('image_id__product_id', flat=True).distinct()
             flower_product_id = flowers.values_list('product_id', flat=True).distinct()
-            result_product_pk = list(set(chain(products_product_id, colors_product_id, flower_product_id)))
+            colors_product_id = colors.prefetch_related('image_id').values_list('image_id__product_id', flat=True).distinct()
 
-            product_result = Product.objects.filter(pk__in=result_product_pk)
+            result_product_pk = list(set(chain(products_product_id, flower_product_id)) - set(colors_product_id))
+
+            filtered_product = Product.objects.filter(pk__in=result_product_pk)
 
     context = {
-        'products': product_result,
+        'filtered_product': filtered_product,
+        'color_filtered': color_filtered,
         'search_term': query,
     }
 
