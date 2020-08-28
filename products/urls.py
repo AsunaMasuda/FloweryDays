bb6c9f1cf0d9c8f78fae2356842f420749d18ca8
Asuna Masuda <asuna.fitz@gmail.com>
2020-08-28 23:34:30 +0000

    rename the root url to onlineshop

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from. import views
 
 urlpatterns = [
-    path('', views.initial_products, name='products'),
+    path('', views.onlineshop, name='onlineshop'),
     path('search_result/', views.all_products, name='products'),
 ]
