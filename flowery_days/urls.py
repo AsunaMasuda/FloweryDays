5b9863ba03726102bb69ebaf245a1a8e4968ce4a
Asuna Masuda <asuna.fitz@gmail.com>
2020-08-30 00:33:33 +0000

    Add path to products.urls in project level urls.py

--- flowery_days/urls.py
+++ flowery_days/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
+    path('products/', include('products.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
