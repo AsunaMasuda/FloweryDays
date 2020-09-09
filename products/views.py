1c910d8dd80fd480a6033f4e99b8309e93e654e7
Asuna Masuda <asuna.fitz@gmail.com>
2020-09-09 22:12:47 +0000

    Add a function that filters products by the select/options in online shop

--- products/views.py
+++ products/views.py
@@ -8,6 +8,63 @@ import operator
 from .models import Product, Image, Color, Flower
 
 
+def filter_product(request):
+    categories = list(set(Product.objects.values_list('category', flat=True)))
+    occasions = list(set(Product.objects.values_list('occasion', flat=True)))
+    colors = Color.objects.all()
+    flowers = Flower.objects.all()
+
+    category_name = request.GET.getlist('filter_category')
+    color_name = request.GET.getlist('filter_color')
+    flower_name = request.GET.getlist('filter_flower')
+    occasion_name = request.GET.getlist('filter_occasion')
+
+    result_product_pk = set()
+
+    if len(category_name) > 0:
+        filtered_category = Product.objects.all().filter(reduce(operator.__or__,
+            [Q(category__icontains=category) for category in category_name]))
+        category_product_id = filtered_category.values_list('pk', flat=True)
+        result_product_pk = set(category_product_id)
+
+    if len(color_name) > 0:
+        filtered_color = colors.filter(reduce(operator.__or__,
+            [Q(name__icontains=color) for color in color_name]))
+        color_product_id = filtered_color.prefetch_related('image_id').values_list('image_id__product_id', flat=True).distinct()
+        if len(result_product_pk) > 0:
+            result_product_pk = result_product_pk & set(color_product_id)
+        else: 
+            result_product_pk = set(color_product_id)
+
+    if len(flower_name) > 0:
+        filtered_flower = flowers.filter(reduce(operator.__or__,
+            [Q(name__icontains=flower) for flower in flower_name]))
+        flower_product_id = filtered_flower.values_list('product_id', flat=True).distinct()
+        if len(result_product_pk) > 0:
+            result_product_pk = result_product_pk & set(flower_product_id)
+        else: 
+            result_product_pk = set(flower_product_id)
+
+    if len(occasion_name) > 0:
+        filtered_occasion = Product.objects.all().filter(reduce(operator.__or__,
+            [Q(occasion__icontains=occasion) for occasion in occasion_name]))
+        occasion_product_id = filtered_occasion.values_list('pk', flat=True)
+        if len(result_product_pk) > 0:
+            result_product_pk = result_product_pk & set(occasion_product_id)
+        else: 
+            result_product_pk = set(occasion_product_id)
+
+    products = Product.objects.filter(pk__in=result_product_pk)
+
+    context = {
+        'categories': categories,
+        'occasions': occasions,
+        'colors': colors,
+        'flowers': flowers,
+        'products': products,
+    }
+
+    return render(request, 'products/products_onlineshop.html', context)
 def onlineshop(request):
     categories = list(set(Product.objects.values_list('category', flat=True)))
     occasions = list(set(Product.objects.values_list('occasion', flat=True)))
