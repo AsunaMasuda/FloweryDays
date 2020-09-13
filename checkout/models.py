9e3b05433da50dc6ea62363b3f4e0b09f536a190
Asuna Masuda <asuna.fitz@gmail.com>
2020-09-13 22:44:18 +0000

    Fix typos in checkout/models.py

--- checkout/models.py
+++ checkout/models.py
@@ -2,28 +2,28 @@ import uuid
 
 from django.db import models
 from django.db.models import Sum
-from dhango.conf import settings
+from django.conf import settings
 
 from products.models import Product
 
 class Order(models.Model):
-    order_number = models.Charfield(max_length=32, null=False, editable=False)
-    first_name = models.Charfield(max_length=32, null=False, blank=False)
-    last_name = models.Charfield(max_length=32, null=False, blank=False)
-    email = models.Emailfield(max_length=254, null=False, blank=False)
-    address_line_1 = models.Charfield(max_length=80, null=False, blank=False)
-    address_line_2 = models.Charfield(max_length=80, null=False, blank=False)
-    town_or_city = models.Charfield(max_length=80, null=False, blank=False)
-    county_or_state = models.Charfield(max_length=80, null=False, blank=False)
-    postcode = models.Charfield(max_length=80, null=False, blank=True)
-    order_date = models.DateTimefield(auto_now_add=True)
-    delivery_cost = models.Decimalfield(max_digit=6, decimal_places=2,
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    first_name = models.CharField(max_length=32, null=False, blank=False)
+    last_name = models.CharField(max_length=32, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    address_line_1 = models.CharField(max_length=80, null=False, blank=False)
+    address_line_2 = models.CharField(max_length=80, null=False, blank=False)
+    town_or_city = models.CharField(max_length=80, null=False, blank=False)
+    county_or_state = models.CharField(max_length=80, null=False, blank=False)
+    postcode = models.CharField(max_length=80, null=False, blank=True)
+    order_date = models.DateTimeField(auto_now_add=True)
+    delivery_cost = models.DecimalField(max_digits=6, decimal_places=2,
                     null=False, default=0)
-    order_total = models.Decimalfield.Decimalfield(max_digit=10,
+    order_total = models.DecimalField(max_digits=10,
                   decimal_places=2, null=False, default=0)
-    grand_total = models.Decimalfield(max_digit=10, decimal_places=2,
+    grand_total = models.DecimalField(max_digits=10, decimal_places=2,
                   null=False, default=0)
-    is_gift_wrapping = models.Booleanfield(default=False)
+    is_gift_wrapping = models.BooleanField(default=False)
 
     def _genearte_order_number(self):
         """
@@ -58,9 +58,9 @@ class Order(models.Model):
 
 class OrderLineItem(models.Model):
     order_id = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
-    product_id = models.ForeisngKey(Product, null=False, blank=False, on_delete=models.CASCADE)
-    quantity = models.IntergerField(null=False, blank=False, default=0)
-    lineitem_total = models.DecimalField(max_digit=6, decimal_places=2, null=False, blank=False, editable=False)
+    product_id = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+    quantity = models.IntegerField(null=False, blank=False, default=0)
+    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
 
     def save(self, *args, **kwargs):
         """
