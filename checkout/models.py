3b38452b98b0c41dfeffb986c164f1690c96834e
Asuna Masuda <asuna.fitz@gmail.com>
2020-09-13 19:07:54 +0000

    Add Order model to models.py

--- checkout/models.py
+++ checkout/models.py
@@ -1,21 +1,22 @@
 from django.db import models
 
-# Create your models here.
 
-user_profile = models.Charfield()
-first_name = models.Charfield()
-last_name = models.Charfield()
-email = models.Emailfield()
-address_line_1 = models.Charfield()
-address_line_2 = models.Charfield()
-town_or_city = models.Charfield()
-county_or_state = models.Charfield()
-postcode = models.Charfield()
-order_date = models.DateTimefield()
-delivery_cost = models.Decimalfield()
-delivery_type = models.Charfield()
-order_total = models.Decimalfield()
-grand_total = models.Decimalfield()
-is_gift_wrapping = models.Booleanfield()
-original_bag = models.Textfield()
-stripe_pid = models.Charfield()
+class Order(models.Model):
+    order_number = models.Charfield(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey()
+    first_name = models.Charfield(max_length=32, null=False, blank=False)
+    last_name = models.Charfield(max_length=32, null=False, blank=False)
+    email = models.Emailfield(max_length=254, null=False, blank=False)
+    address_line_1 = models.Charfield(max_length=80, null=False, blank=False)
+    address_line_2 = models.Charfield(max_length=80, null=False, blank=False)
+    town_or_city = models.Charfield(max_length=80, null=False, blank=False)
+    county_or_state = models.Charfield(max_length=80, null=False, blank=False)
+    postcode = models.Charfield(max_length=80, null=False, blank=True)
+    order_date = models.DateTimefield(auto_now_add=True)
+    delivery_cost = models.Decimalfield(max_digit=6, decimal_places=2,
+                    null=False, default=0)
+    order_total = models.Decimalfield.Decimalfield(max_digit=10,
+                  decimal_places=2, null=False, default=0)
+    grand_total = models.Decimalfield(max_digit=10, decimal_places=2,
+                  null=False, default=0)
+    is_gift_wrapping = models.Booleanfield()
