d64f4279ea9e35e8df10c20db5df146708cf5f30
Asuna Masuda <asuna.fitz@gmail.com>
2020-09-15 21:29:11 +0000

    Add url path to cart app to urls.py

--- flowery_days/urls.py
+++ flowery_days/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('products/', include('products.urls')),
+    path('cart/', include('cart.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
