ae1906bb9d54b9a00de3a93abc7f832ac8c065c3
Asuna Masuda <asuna.fitz@gmail.com>
2020-09-20 21:35:31 +0000

    Add adjust_Cart and remove_from_cart functions to views.py

--- cart/views.py
+++ cart/views.py
@@ -1,13 +1,23 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse,get_object_or_404
+from django.contrib import messages
+from products.models import Product
 
 
 def view_cart(request):
     return render(request, 'cart/cart.html')
 
+
 def add_to_cart(request, item_id):
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+
+    if request.POST.get('quantity'):
+        quantity = int(request.POST.get('quantity'))
+    else:
+        quantity = 1
+
     cart = request.session.get('cart', {})
 
     if item_id in list(cart.keys()):
@@ -16,5 +26,26 @@ def add_to_cart(request, item_id):
         cart[item_id] = quantity
 
     request.session['cart'] = cart
-    print(request.session['cart'])
     return redirect(redirect_url)
+
+
+def adjust_cart(request, item_id):
+
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+    cart[item_id] = quantity
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
+
+
+def remove_from_cart(request, item_id):
+    try:
+        cart = request.session.get('cart', {})
+        cart.pop(item_id)
+
+        request.session['cart'] = cart
+        return HttpResponse(status=200)
+
+    except Exception:
+        return HttpResponse(status=500)
