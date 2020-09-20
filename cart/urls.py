1aaad0ac903eaf5eec7721e712ff7d24f334c4d6
Asuna Masuda <asuna.fitz@gmail.com>
2020-09-20 21:34:23 +0000

    Add paths if adjust and remove in cart urls.py

--- cart/urls.py
+++ cart/urls.py
@@ -4,5 +4,7 @@ from. import views
 
 urlpatterns = [
     path('', views.view_cart, name='view_cart'),
-    path('add/<item_id>', views.add_to_cart, name='add_to_cart')
+    path('add/<item_id>/', views.add_to_cart, name='add_to_cart'),
+    path('adjust/<item_id>/', views.adjust_cart, name='adjust_cart'),
+    path('remove/<item_id>/', views.remove_from_cart, name='remove_from_cart'),
 ]
