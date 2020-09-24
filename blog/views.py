bb92cbd4ac820ccf7528981f06b7ebf5897ff6e3
Asuna Masuda <asuna.fitz@gmail.com>
2020-09-24 23:35:02 +0000

    Add a view for blog.html to blog/views.py

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
+from .models import BlogPost, BlogImage, BlogComments
+
+def blog_feed(request):
+
+    blog_post = BlogPost.objects.all()
+    blog_image = BlogImage.objects.all()
+    blog_comments = BlogComments.objects.all()
+
+    # Pagination
+    paginator = Paginator(blog_post, 5)
+    page = request.GET.get('page')
+
+    try:
+        blog_post = paginator.page(page)
+    except PageNotAnInteger:
+        blog_post = paginator.page(1)
+    except EmptyPage:
+        blog_post = paginator.page(paginator.num_pages)
+
+    context = {
+        'blog_post': blog_post,
+        'blog_image': blog_image,
+        'blog_comments': blog_comments,
+    }
+
+    return render(request, 'blog/blog.html', context)
