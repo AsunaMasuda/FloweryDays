b0d425d306cef3b6074f72e409fdbf56bc9046d2
Asuna Masuda <asuna.fitz@gmail.com>
2020-09-24 23:33:14 +0000

    Fix the list_display attributes

--- blog/admin.py
+++ blog/admin.py
@@ -1,21 +1,23 @@
 from django.contrib import admin
 from .models import BlogPost, BlogImage, BlogComments
- 
- 
+
+
 class BlogAdmin(admin.ModelAdmin):
-   list_display = ('title', 'slug', 'status','post_created_date')
-   list_filter = ("status",)
-   search_fields = ['title', 'content']
-   prepopulated_fields = {'slug': ('title',)}
- 
+    list_display = ('title', 'slug', 'status','created_on')
+    list_filter = ("status",)
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+
+
 class PostImageAdmin(admin.ModelAdmin):
-   list_display = ('image', 'image_article_id')
- 
+    list_display = ('image', 'article_id')
+
+
 class BlogCommentsAdmin(admin.ModelAdmin):
-   list_display = ('comment_article_id', 'user_name', 'blog_comment', 'created_on')
-   search_fields = ['blog_comment']
- 
- 
-admin.site.register(BlogPost)
-admin.site.register(BlogImage)
-admin.site.register(BlogComments)
+    list_display = ('article_id', 'user_name', 'blog_comment', 'created_on')
+    search_fields = ['blog_comment']
+
+
+admin.site.register(BlogPost, BlogAdmin)
+admin.site.register(BlogImage, PostImageAdmin)
+admin.site.register(BlogComments, BlogCommentsAdmin)
