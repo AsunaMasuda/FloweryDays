73ae5218455330648da46b7e05bf6bbc33dd6d62
Asuna Masuda <asuna.fitz@gmail.com>
2020-09-29 22:20:03 +0000

    Add pathes to category_view and post_view

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.blog_feed, name='blog'),
+    path('category_view/<str:category>/', views.category_view, name='category_view'),
+    path('blog_detail/<slug:slug>/', views.post_view, name='post_view'),
+]
