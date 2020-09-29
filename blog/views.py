500246b7cde342e590083ea39192a665ff553012
Asuna Masuda <asuna.fitz@gmail.com>
2020-09-29 22:20:50 +0000

    Add a view for post_category_view.html to views.py

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,20 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
 from .models import BlogPost, BlogImage, BlogComments
+
 
 def blog_feed(request):
     #order by newest first for blog_post
     blog_post = BlogPost.objects.all().order_by('-created_on')
     blog_image = BlogImage.objects.all()
-    blog_comments = BlogComments.objects.all()
+
+    category_dict = dict()
+    for key in blog_post:
+        if key.category in category_dict.keys():
+            category_dict[key.category] += 1
+        else:
+            category_dict[key.category] = 1
 
     # Pagination
     paginator = Paginator(blog_post, 5)
@@ -23,7 +30,41 @@ def blog_feed(request):
     context = {
         'blog_post': blog_post,
         'blog_image': blog_image,
-        'blog_comments': blog_comments,
+        'category_dict': category_dict,
     }
 
     return render(request, 'blog/blog.html', context)
+
+
+def category_view(request, category):
+    selected_blog = BlogPost.objects.all().filter(category__icontains=category)
+    blog_image = BlogImage.objects.all()
+    blog_post = BlogPost.objects.all()
+
+    category_dict = dict()
+    for key in blog_post:
+        if key.category in category_dict.keys():
+            category_dict[key.category] += 1
+        else:
+            category_dict[key.category] = 1
+
+    context = {
+        'selected_blog': selected_blog,
+        'blog_image': blog_image,
+        'category_dict': category_dict,
+        'category': category,
+    }
+
+    return render(request, 'blog/post_category_view.html', context)
+
+
+def post_view(request, slug):
+    blog_post = BlogPost.objects.get(slug=slug)
+    blog_image = BlogImage.objects.get(article_id=blog_post.id)
+    
+    context = {
+        'blog_post': blog_post,
+        'blog_image': blog_image,
+    }
+    
+    return render(request, 'blog/post_detail.html', context)
