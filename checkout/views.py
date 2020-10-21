be932a652799491bc104e50711334c0c01046a94
Asuna Masuda <asuna.fitz@gmail.com>
2020-10-21 21:59:43 +0000

    Add view:checkout to views.py

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
-# Create your views here.
+from .forms import OrderForm
+
+
+def checkout(request):
+    cart = request.session.get('cart', {})
+    if not cart:
+        messages.error(request, "There is no item in your cart.")
+        redirect(reverse('onlineshop'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
