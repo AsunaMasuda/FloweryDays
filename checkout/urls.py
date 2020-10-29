9260fb8f485b40c50b93610d7e2b467633c46c90
Asuna Masuda <asuna.fitz@gmail.com>
2020-10-29 22:23:05 +0000

    Add checkout_success

--- checkout/urls.py
+++ checkout/urls.py
@@ -3,4 +3,6 @@ from. import views
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
-]
+    path('checkout_success/<order_number>', views.checkout_success,
+         name='checkout_success'),
+         ]
