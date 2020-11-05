db1dcfbb4ea41d66c18aea9858dae0731c947edf
Asuna Masuda <asuna.fitz@gmail.com>
2020-11-05 21:59:23 +0000

    Add cache_checkout_data to checkout/urls.py

--- checkout/urls.py
+++ checkout/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('checkout_success/<order_number>', views.checkout_success,
          name='checkout_success'),
+    path('cache_checkout_data/', views.cache_checkout_data,
+         name='cache_checkout_data'),
     path('wh/', webhook, name='webhook'),
          ]
