c532d4f2d4c1f5a19a43096472fa7b34bafb319d
Asuna Masuda <asuna.fitz@gmail.com>
2020-11-08 20:09:51 +0000

    Add profile view to views.py

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import UserProfile
+
+def profile(request):
+    """ Display the user's profile. """
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    template = 'profiles/profile.html'
+    context = {
+        'profile' : profile,
+    }
+
+    return render(request, template, context)
