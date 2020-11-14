acfa2e63178260aee7a63283b7536fe16645563f
Asuna Masuda <asuna.fitz@gmail.com>
2020-11-14 19:48:57 +0000

    Fix 'phone_number' to 'email' in models.py

--- profiles/models.py
+++ profiles/models.py
@@ -10,7 +10,7 @@ class UserProfile(models.Model):
     delivery information and order history
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    default_phone_number = models.CharField(max_length=20, null=True, blank=True)
+    default_email = models.EmailField(max_length=254, null=True, blank=False)
     default_postcode = models.CharField(max_length=20, null=True, blank=True)
     default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
     default_address_line_1 = models.CharField(max_length=80, null=True, blank=True)
