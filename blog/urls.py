3cd62dfee824a06b4efc63b098ccc2217471fd01
Asuna Masuda <asuna.fitz@gmail.com>
2020-11-20 22:52:01 +0000

    Fix the format in blog/urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.blog_feed, name='blog'),
-    path('category_view/<str:category>/', views.category_view, name='category_view'),
+    path('category_view/<str:category>/', views.category_view,
+         name='category_view'),
     path('blog_detail/<slug:slug>/', views.post_view, name='post_view'),
 ]
