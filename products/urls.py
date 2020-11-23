72bb4a8f8ac855d7a14aad3e6ecd90ce3eef2ec2
Asuna Masuda <asuna.fitz@gmail.com>
2020-11-23 19:21:33 +0000

    Add path for delete_product in urls.py

--- products/urls.py
+++ products/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('filter_result/', views.filter_product, name='filter_product'),
     path('add/', views.add_product, name='add_product'),
     path('edit/<int:product_pk>/', views.edit_product, name='edit_product'),
+    path('delete/<int:product_pk>/', views.delete_product,
+         name='delete_product'),
 ]
