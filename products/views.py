23145fc146307865c880ab9cec3954f51cab5fb4
Asuna Masuda <asuna.fitz@gmail.com>
2020-12-08 23:22:27 +0000

    Add context for product review

--- products/views.py
+++ products/views.py
@@ -6,10 +6,11 @@ from django.db.models import Q
 from itertools import chain
 from functools import reduce
 import operator
+from django.db.models import Avg
 
-from .models import Product, Image, Color, Flower
+from .models import Product, Image, Color, Flower, ProductReview
 
-from .forms import ProductForm
+from .forms import ProductForm, ProductReviewForm
 
 
 def filter_product(request):
@@ -29,35 +30,37 @@ def filter_product(request):
 
     if len(category_name) > 0:
         filtered_category = Product.objects.all().filter(reduce(operator.__or__,
-            [Q(category__icontains=category) for category in category_name]))
+                                                        [Q(category__icontains=category) for category in category_name]))
         category_product_id = filtered_category.values_list('pk', flat=True)
         result_product_pk = set(category_product_id)
 
     if len(color_name) > 0:
         filtered_color = colors.filter(reduce(operator.__or__,
-            [Q(name__icontains=color) for color in color_name]))
-        color_product_id = filtered_color.prefetch_related('image_id').values_list('image_id__product_id', flat=True).distinct()
+                                              [Q(name__icontains=color) for color in color_name]))
+        color_product_id = filtered_color.prefetch_related(
+            'image_id').values_list('image_id__product_id', flat=True).distinct()
         if len(result_product_pk) > 0:
             result_product_pk = result_product_pk & set(color_product_id)
-        else: 
+        else:
             result_product_pk = set(color_product_id)
 
     if len(flower_name) > 0:
         filtered_flower = flowers.filter(reduce(operator.__or__,
-            [Q(name__icontains=flower) for flower in flower_name]))
-        flower_product_id = filtered_flower.values_list('product_id', flat=True).distinct()
+                                                [Q(name__icontains=flower) for flower in flower_name]))
+        flower_product_id = filtered_flower.values_list(
+            'product_id', flat=True).distinct()
         if len(result_product_pk) > 0:
             result_product_pk = result_product_pk & set(flower_product_id)
-        else: 
+        else:
             result_product_pk = set(flower_product_id)
 
     if len(occasion_name) > 0:
         filtered_occasion = Product.objects.all().filter(reduce(operator.__or__,
-            [Q(occasion__icontains=occasion) for occasion in occasion_name]))
+                                                                [Q(occasion__icontains=occasion) for occasion in occasion_name]))
         occasion_product_id = filtered_occasion.values_list('pk', flat=True)
         if len(result_product_pk) > 0:
             result_product_pk = result_product_pk & set(occasion_product_id)
-        else: 
+        else:
             result_product_pk = set(occasion_product_id)
 
     product_list = Product.objects.filter(pk__in=result_product_pk)
@@ -89,13 +92,15 @@ def filter_product(request):
 
 
 def onlineshop(request):
+    """  """
+
     categories = list(set(Product.objects.values_list('category', flat=True)))
     occasions = list(set(Product.objects.values_list('occasion', flat=True)))
     colors = Color.objects.all()
     flowers = Flower.objects.all()
     product_list = Product.objects.all()
 
-    # Default filter
+    # Default filter when visitors accessing online shop
     category_name = ['bouquet']
     product_list = product_list.filter(category='bouquet')
 
@@ -127,12 +132,38 @@ def single_product(request, product_pk):
 
     product = get_object_or_404(Product, pk=product_pk)
     images = Image.objects.filter(product_id_id=product_pk)
-    colors = Image.objects.prefetch_related("color_set").filter(product_id_id=product_pk)
+    colors = Image.objects.prefetch_related(
+        "color_set").filter(product_id_id=product_pk)
+    product_reviews = ProductReview.objects.filter(product_id_id=product_pk)
+
+    if product_reviews:
+        average_score = product_reviews.all().aggregate(Avg('rating_score'))
+        average_score_percentage = average_score['rating_score__avg']/5*100
+    else:
+        average_score = None
+        average_score_percentage = None
+
+    if request.method == 'POST':
+        product_review_form = ProductReviewForm(data=request.POST)
+        if product_review_form.is_valid():
+            # Create Review object but don't save to database yet
+            new_review = product_review_form.save(commit=False)
+            # Assign the current review to the product
+            new_review.product_id = product
+            # Save the review to the database
+            new_review.save()
+            product_review_form = ProductReviewForm()
+    else:
+        product_review_form = ProductReviewForm()
 
     context = {
         'product': product,
         'colors': colors,
         'images': images,
+        'product_reviews': product_reviews,
+        'product_review_form': product_review_form,
+        'average_score': average_score,
+        'average_score_percentage': average_score_percentage,
     }
 
     return render(request, 'products/single_product.html', context)
@@ -153,7 +184,8 @@ def add_product(request):
             messages.success(request, 'You added the product successfully!')
             return redirect(reverse('single_product', args=[product.id]))
         else:
-            messages.error(request, 'Failed to add the product. Please ensure the form is valid.')
+            messages.error(
+                request, 'Failed to add the product. Please ensure the form is valid.')
     form = ProductForm()
     template = 'products/add_product.html'
     context = {
@@ -180,11 +212,13 @@ def edit_product(request, product_pk):
             messages.success(request, f'You updated {product.name}!')
             return redirect(reverse('single_product', args=[product_pk]))
         else:
-            messages.error(request, 'Failed to update product. Please ensure the form is valid.')
-    
+            messages.error(
+                request, 'Failed to update product. Please ensure the form is valid.')
+
     else:
         form = ProductForm(instance=product)
-        messages.info(request, f'You are editing a product details: {product.name}')
+        messages.info(
+            request, f'You are editing a product details: {product.name}')
 
     template = 'products/edit_product.html'
     context = {
@@ -209,41 +243,60 @@ def delete_product(request, product_pk):
     return redirect(reverse('onlineshop'))
 
 
-def all_products(request):
-    """ A view to show all products, including sorting and search queries """
+def search_result(request):
+    """A view to render the results by keyword search"""
 
     products = Product.objects.all()
     colors = Color.objects.all()
     flowers = Flower.objects.all()
-    query = None
+    categories = list(set(Product.objects.values_list('category', flat=True)))
+    occasions = list(set(Product.objects.values_list('occasion', flat=True)))
+    queries = None
 
-    if request.GET:
-        if 'q' in request.GET:
-            queries = request.GET['q'].split()
-            if not queries:
-             messages.error(request, 
-            f'Please enter keywords to search a product')
-            
-            colors =  colors.filter(reduce(operator.__or__, [Q(name__icontains=query) for query in queries]))
-            flowers =  flowers.filter(reduce(operator.__or__, [Q(name__icontains=query) for query in queries]))
-            products =  products.filter(reduce(operator.__or__, [Q(name__icontains=query) | Q(description__icontains=query) for query in queries]))
+    if request.GET['q']:
+        queries = request.GET['q'].split()
 
-            # QuerySet with images of a searched color
-            color_filtered = colors.prefetch_related('image_id__product_id')
+        filtered_colors = colors.filter(
+            reduce(operator.__or__, [Q(name__icontains=query) for query in queries]))
+        filtered_flowers = flowers.filter(
+            reduce(operator.__or__, [Q(name__icontains=query) for query in queries]))
+        filtered_products_name = products.filter(reduce(operator.__or__, [Q(
+            name__icontains=query) | Q(description__icontains=query) for query in queries]))
 
-            # Getting pk of product table from all the filters
-            products_product_id = products.values_list('pk', flat=True)
-            flower_product_id = flowers.values_list('product_id', flat=True).distinct()
-            colors_product_id = colors.prefetch_related('image_id').values_list('image_id__product_id', flat=True).distinct()
+        # QuerySet with images of a searched color
+        color_filtered = colors.prefetch_related('image_id__product_id')
 
-            result_product_pk = list(set(chain(products_product_id, flower_product_id)) - set(colors_product_id))
+        # Getting pk of product table from all the filters
+        products_product_id = filtered_products_name.values_list(
+            'pk', flat=True)
+        flower_product_id = filtered_flowers.values_list(
+            'product_id', flat=True).distinct()
+        colors_product_id = filtered_colors.prefetch_related(
+            'image_id').values_list('image_id__product_id', flat=True).distinct()
 
-            filtered_product = Product.objects.filter(pk__in=result_product_pk)
+        result_product_pk = list(
+            set(chain(products_product_id, flower_product_id)) - set(colors_product_id))
+
+        products = Product.objects.filter(pk__in=result_product_pk)
+
+    # Pagination
+    paginator = Paginator(products, 9)
+    page = request.GET.get('page')
+
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
 
     context = {
-        'filtered_product': filtered_product,
-        'color_filtered': color_filtered,
-        'search_term': query,
+        'products': products,
+        'colors': colors,
+        'flowers': flowers,
+        'categories': categories,
+        'occasions': occasions,
+        'queries': queries,
     }
 
     return render(request, 'products/products_onlineshop.html', context)
