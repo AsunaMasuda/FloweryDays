c30313bd3ce5c05306da14cc474109d7ee11d186
Asuna Masuda <asuna.fitz@gmail.com>
2020-12-13 23:43:34 +0000

    Change comment field and admin list display according to the change of the fields in the BlogComments model

--- blog/forms.py
+++ blog/forms.py
@@ -5,4 +5,4 @@ from django import forms
 class CommentForm(forms.ModelForm):
     class Meta:
         model = BlogComments
-        fields = ('comment_name', 'blog_comment')
+        fields = ('comment_title', 'blog_comment')
