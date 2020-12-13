98564a7dfe6be2371068d970f90a612f4d5bce96
Asuna Masuda <asuna.fitz@gmail.com>
2020-12-13 23:51:56 +0000

    Add a new line at the end

--- cart/contexts.py
+++ cart/contexts.py
@@ -4,7 +4,7 @@ from django.shortcuts import get_object_or_404
 from products.models import Product
 
 
-def cart_contexts(request):
+def cart_contents(request):
 
     cart_items = []
     total = 0
--- profiles/views.py
+++ profiles/views.py
@@ -36,11 +36,6 @@ def profile(request):
 def order_history(request, order_number):
     order = get_object_or_404(Order, order_number=order_number)
 
-    messages.info(request, (
-        f'This is a past confirmation for order number {order_number}.'
-        'A confirmation email was sent on the order date.'
-    ))
-
     template = 'checkout/checkout_success.html'
     context = {
         'order': order,
