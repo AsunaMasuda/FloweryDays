c4f4049feddbf7e63c2fc9d0add087d09a146b99
Asuna Masuda <asuna.fitz@gmail.com>
2020-12-14 21:01:09 +0000

    Add a comment to explain in_image template tag

--- blog/templatetags/blog_image_filter.py
+++ blog/templatetags/blog_image_filter.py
@@ -6,4 +6,5 @@ register = template.Library()
 
 @register.filter(name='in_image')
 def in_image(blog_image, pk):
+    """A template tag to render post image in the specified layout"""
     return blog_image.filter(article_id=pk)
