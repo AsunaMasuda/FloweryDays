9d0f5fc1c79260f74cc8a6bc2995488c0137d1a9
Asuna Masuda <asuna.fitz@gmail.com>
2020-12-14 21:19:22 +0000

    Beautify code and format

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from .models import Order, OrderLineItem
 
+
 class OrderLineItemAdminInline(admin.TabularInline):
     """
-    This inline item is going to allow us to add and edit line items in the admin
-    right from inside the order model.
+    This inline item is going to allow us to add and edit line
+    items in theadmin right from inside the order model.
     """
-    model = OrderLineItem # from models.py
+    model = OrderLineItem
     readonly_fields = ('lineitem_total',)
+
 
 class OrderAdmin(admin.ModelAdmin):
     inlines = (OrderLineItemAdminInline,)
@@ -18,11 +20,14 @@ class OrderAdmin(admin.ModelAdmin):
     fields = ('order_number', 'user_profile', 'order_date', 'first_name',
               'last_name', 'email', 'address_line_1', 'address_line_2',
               'postcode', 'town_or_city', 'county', 'delivery_cost',
-              'order_total', 'grand_total', 'is_gift_wrapping', 'original_cart', 'stripe_pid')
+              'order_total', 'grand_total', 'is_gift_wrapping',
+              'original_cart', 'stripe_pid')
 
     list_display = ('order_number', 'order_date', 'first_name',
-              'last_name', 'order_total', 'delivery_cost', 'grand_total',)
+                    'last_name', 'order_total', 'delivery_cost',
+                    'grand_total',)
 
     ordering = ('-order_date',)
+
 
 admin.site.register(Order, OrderAdmin)
