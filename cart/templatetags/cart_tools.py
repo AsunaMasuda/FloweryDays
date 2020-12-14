b31229519629a0493334a7875dd57e5d168cfe19
Asuna Masuda <asuna.fitz@gmail.com>
2020-12-14 21:10:18 +0000

    Add a comment to explain the tempalte tag cart_tools.py

--- cart/templatetags/cart_tools.py
+++ cart/templatetags/cart_tools.py
@@ -3,6 +3,9 @@ from django import template
 
 register = template.Library()
 
+
 @register.filter(name='calc_subtotal')
 def calc_subtotal(price, quantity):
+    """A template tag to calculate multiple price by quantity"""
+
     return price * quantity
