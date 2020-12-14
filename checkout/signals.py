d3e935ef117720d2cca0f0517f48c342f57dff5c
Asuna Masuda <asuna.fitz@gmail.com>
2020-12-14 22:00:27 +0000

    Add coment and the a new line at the end

--- checkout/signals.py
+++ checkout/signals.py
@@ -6,6 +6,9 @@ from .models import OrderLineItem
 
 @receiver(post_save, sender=OrderLineItem)
 def update_on_save(sender, instance, created, **kwargs):
+    """
+    Update order total on lineitem save
+    """
     instance.order.update_total()
 
 
