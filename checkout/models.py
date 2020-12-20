5aa12c8242075f80bfdf5563afce9ed250336f1e
Asuna Masuda <asuna.fitz@gmail.com>
2020-12-20 23:36:23 +0000

    Fix format of long line|

--- checkout/models.py
+++ checkout/models.py
@@ -45,8 +45,12 @@ class Order(models.Model):
         accouting for delivery cost.
         """
 
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))
-        ['lineitem_total__sum'] or 0
+        self.order_total = (self
+                            .lineitems
+                            .aggregate
+                            (Sum('lineitem_total'))
+                            ['lineitem_total__sum']) or 0
+
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = (self.order_total *
                                   settings.STANDARD_DELIVERY_PERCENTAGE) / 100
