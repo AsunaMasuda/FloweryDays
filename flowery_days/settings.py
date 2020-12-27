bda2104ca559c462e20969e418815c47f1efc1e0
Asuna Masuda <asuna.fitz@gmail.com>
2020-12-27 23:39:01 +0000

    Remove secret key and set debug

--- flowery_days/settings.py
+++ flowery_days/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 's#an7*qntj8fb!($qx=6ij)1=fqp)&x(s3%zpl69c4qohkfc!-'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['flowerydays', 'localhost']
 
