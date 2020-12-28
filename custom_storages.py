06f11db6e0ae6b6229638d89c604c89387ae4346
Asuna Masuda <asuna.fitz@gmail.com>
2020-12-28 21:28:30 +0000

    Fix Typo

--- custom_storages.py
+++ custom_storages.py
@@ -7,4 +7,4 @@ class StaticStorage(S3Boto3Storage):
 
 
 class MediaStorage(S3Boto3Storage):
-    location = settings.STATICFILES_LOCATION
+    location = settings.MEDIAFILES_LOCATION
--- flowery_days/settings.py
+++ flowery_days/settings.py
@@ -76,7 +76,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             os.path.join(BASE_DIR, 'templates'),
-            os.path.join(BASE_DIR, 'templates', 'allauth')
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -191,17 +191,13 @@ if 'USE_AWS' in os.environ:
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
     STATICFILES_LOCATION = 'static'
-    DEFAULT_FILE_STORAGE = 'custom_storage.MediaStorage'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
 
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
-
-INTERNAL_IPS = [
-    '127.0.0.1',
-]
 
 # Stripe
 FREE_DELIVERY_THRESHOLD = 50
