cdcb497bc52516774d44865fc2f37738c04d0365
Asuna Masuda <asuna.fitz@gmail.com>
2020-12-28 20:51:18 +0000

    Fix the name for ALLOWED_HOST

--- flowery_days/settings.py
+++ flowery_days/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['flowerydays', 'localhost']
+ALLOWED_HOSTS = ['flowerydays.herokuapp.com', 'localhost']
 
 
 # Application definition
