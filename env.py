29deb3cf8fa262f12edb24c9e11a9deb40aff7a6
Asuna Masuda <asuna.fitz@gmail.com>
2020-12-29 00:57:25 +0000

    Add env.py

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+# Secret Key Value:
+os.environ.setdefault("SECRET_KEY",
+                      "5)4r0!_ahagq%0jlgm$wr+i*--$i9i0mydq1&&uymmp9f$_+55")
--- flowery_days/settings.py
+++ flowery_days/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import dj_database_url
+if os.path.exists("env.py"):
+    import env
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -22,12 +24,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['flowerydays.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['flowerydays.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
