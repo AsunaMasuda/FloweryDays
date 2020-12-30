7edcd04124c5cc18848bf53b5dd5a1f1b1e02c90
Asuna Masuda <asuna.fitz@gmail.com>
2020-12-30 23:53:54 +0000

    Modify cart view and context to include color options

--- cart/contexts.py
+++ cart/contexts.py
@@ -13,15 +13,27 @@ def cart_contents(request):
     cart = request.session.get('cart', {})
     last_item = request.session.get('last_item', {})
 
-    for item_id, quantity in cart.items():
-        product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
-        product_count += quantity
-        cart_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-        })
+    for item_id, item_data in cart.items():
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
+            cart_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            for color, quantity in item_data['items_by_color'].items():
+                total += quantity * product.price
+                product_count += quantity
+                cart_items.append({
+                    'item_id': item_id,
+                    'quantity': quantity,
+                    'product': product,
+                    'color': color,
+                })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE/100)
--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse,get_object_or_404
+from django.shortcuts import (render, redirect, reverse,
+                              HttpResponse, get_object_or_404)
 from django.contrib import messages
-from products.models import Product
+from products.models import Product, Image, Color
 
 
 def view_cart(request):
@@ -14,26 +15,58 @@ def add_to_cart(request, item_id):
 
     product = get_object_or_404(Product, pk=item_id)
     redirect_url = request.POST.get('redirect_url')
+    color = None
+
+    if 'color' in request.POST:
+        color = request.POST['color']
+    cart = request.session.get('cart', {})
 
     if request.POST.get('quantity'):
         quantity = int(request.POST.get('quantity'))
     else:
         quantity = 1
 
-    cart = request.session.get('cart', {})
-    last_item = request.session.get('last_item', {})
+    if color:
+        if item_id in list(cart.keys()):
+            if color in cart[item_id]['items_by_color'].keys():
+                cart[item_id]['items_by_color'][color] += quantity
+                messages.success(request, f'Updated quantity to \
+                                 {cart[item_id]["items_by_color"][color]} for \
+                                 {product.name} / color:{color.upper()}')
+            else:
+                cart[item_id]['items_by_color'][color] = quantity
+                messages.success(request, f'Added {product.name} / \
+                                 color: {color} to your bag')
+        else:
+            cart[item_id] = {'items_by_color': {color: quantity}}
+            messages.success(request, f'Added {product.name} / \
+                             color: {color} to your bag')
+        # Get the image for toast when the product has a color choice
+        images = Image.objects.all()
+        colors = Color.objects.all()
+        product_by_image = images.filter(product_id=product)
+        product_by_color = colors.filter(name=color)
+        result_image_pk = list(set(product_by_image.values_list('pk'))
+                               .intersection(product_by_color
+                               .values_list('image_id')))
+        result_image = get_object_or_404(Image, pk=result_image_pk[0][0])
 
-    if item_id in list(cart.keys()):
-        cart[item_id] += quantity
-        messages.success(request, f"You've updated {product.name} quantity to {cart[item_id]}.")
+        request.session['last_item'] = {'name': product.name,
+                                        'image': result_image.URL}
+
     else:
-        cart[item_id] = quantity
-        messages.success(request, f"You've added {product.name} to your cart.")
+        if item_id in list(cart.keys()):
+            cart[item_id] += quantity
+            messages.success(request, f'Updated {product.name} \
+                             quantity to {cart[item_id]}')
+        else:
+            cart[item_id] = quantity
+            messages.success(request, f'Added {product.name} to your bag')
+        # Get the image for toast when the product does not have a color choice
+        request.session['last_item'] = {'name': product.name,
+                                        'image': product.product_image.url}
 
     request.session['cart'] = cart
-    request.session['last_item'] = {'name': product.name,
-                                    'image': product.product_image.url}
-
     return redirect(redirect_url)
 
 
@@ -45,8 +78,8 @@ def adjust_cart(request, item_id):
     quantity = int(request.POST.get('quantity'))
     cart = request.session.get('cart', {})
     cart[item_id] = quantity
-    messages.success(request, 
-    f"You've updated {product.name} quantity to {cart[item_id]}.")
+    messages.success(request, f"You've updated {product.name} \
+                     quantity to {cart[item_id]}.")
 
     request.session['cart'] = cart
     request.session['last_item'] = {'name': product.name,
@@ -61,13 +94,16 @@ def remove_from_cart(request, item_id):
         product = get_object_or_404(Product, pk=item_id)
         cart = request.session.get('cart', {})
         cart.pop(item_id)
-        messages.success(request, f"You've removed {product.name} from your cart")
+        messages.success(request, f"You've removed \
+                         {product.name} from your cart")
 
         request.session['cart'] = cart
         request.session['last_item'] = {'name': product.name,
-                                        'image': product.product_image.url}
+                                        'image': image.filter(category='bouquet').filter(category='bouquet')
+                                        }
         return HttpResponse(status=200)
 
     except Exception as e:
-        messages.error(request, f'Error occured. When removing {product.name} from your cart')
+        messages.error(request, f'Error occured. When removing \
+                       {product.name} from your cart')
         return HttpResponse(status=500)
