42ded51ba6b12c8c07b65c7cd41bd0309732baf0
Asuna Masuda <asuna.fitz@gmail.com>
2020-12-31 00:22:31 +0000

    Add color column and image for each color option

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,7 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
 from django.shortcuts import get_object_or_404
-from products.models import Product
+from products.models import Product, Image, Color
 
 
 def cart_contents(request):
@@ -28,11 +28,23 @@ def cart_contents(request):
             for color, quantity in item_data['items_by_color'].items():
                 total += quantity * product.price
                 product_count += quantity
+                # Get the image when the product has a color choice
+                images = Image.objects.all()
+                colors = Color.objects.all()
+                product_by_image = images.filter(product_id=product)
+                product_by_color = colors.filter(name=color)
+                result_image_pk = list(set(product_by_image.values_list('pk'))
+                                       .intersection(product_by_color
+                                       .values_list('image_id')))
+                product_image = get_object_or_404(Image,
+                                                  pk=result_image_pk[0][0])
+
                 cart_items.append({
                     'item_id': item_id,
                     'quantity': quantity,
                     'product': product,
                     'color': color,
+                    'product_image': product_image.URL,
                 })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
