c5ccad84d75b2db2495f5b6f1e6001d37a0f4914
Asuna Masuda <asuna.fitz@gmail.com>
2021-01-02 01:25:45 +0000

    Integrate quantity counter to one

--- static/js/base.js
+++ static/js/base.js
@@ -27,41 +27,32 @@ $(window).scroll(function () {
   }
 });
 
-// Quantity Counter
+// Quantity Counter - Cart View
 $(document).ready(function(){
     $(document).on('click', '.plus', function(){
-        let id = this.id.split("_")[1]
-        let color = this.id.split("_")[2]
-        let plus_id = `#number_${id}_${color}`
-        $(plus_id).val(parseInt($(plus_id).val()) + 1 );
-            if ($(plus_id).val() == 100) {
-                $(plus_id).val(99);
+        $(this).siblings('.qty_input').val(parseInt($(this).siblings('.qty_input').val()) + 1);
+            if ($(this).siblings('.qty_input').val() == 100) {
+                $(this).siblings('.qty_input').val(99);
             }
     });
     $(document).on('click', '.minus', function(){
-        let id = this.id.split("_")[1]
-        let color = this.id.split("_")[2]
-        let minus_id = `#number_${id}_${color}`
-        $(minus_id).val(parseInt($(minus_id).val()) - 1 );
-            if ($(minus_id).val() == 0) {
-                $(minus_id).val(1);
+        $(this).siblings('.qty_input').val(parseInt($(this).siblings('.qty_input').val()) - 1);
+            if ($(this).siblings('.qty_input').val() == 0) {
+                $(this).siblings('.qty_input').val(1);
             }
     });
-    $(document).on('change', 'input', function() {
-        let id = this.id.split("_")[1]
-        let color = this.id.split("_")[2]
-        let quantityId = `#number_${id}_${color}`
-        $(quantityId).val(parseInt($(quantityId).val()));
-            if (($(quantityId).val() > 99)) {
-                $('#errorMsg').show();
-                $(quantityId).val(99);
+    $(document).on('change', '.quantity-count', function() {
+        $(this).val($(this).val());
+            if (($(this).val() > 99)) {
+                $('.errorMsg').show();
+                $(this).val(99);
             }
-            else if (($(quantityId).val() < 1 )) {
-                $('#errorMsg').show();
-                $(quantityId).val(1);
+            else if (($(this).val() < 1 )) {
+                $('.errorMsg').show();
+                $(this).val(1);
             }
             else{
-                $('#errorMsg').hide();
+                $('.errorMsg').hide();
             }
     });
 });
