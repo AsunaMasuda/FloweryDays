ebfa1b4fee79cb6db45ad54ee83a97f73f7889e9
Asuna Masuda <asuna.fitz@gmail.com>
2021-01-09 12:28:34 +0000

    Add edit/delete blog form and template

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,17 @@
-from .models import BlogComment
+from .models import BlogPost, BlogImage, BlogComment
 from django import forms
+
+
+class BlogForm(forms.ModelForm):
+    class Meta:
+        model = BlogPost
+        exclude = ('slug', 'created_on', 'status', 'updated_on', 'author')
+
+
+class BlogImageForm(forms.ModelForm):
+    class Meta:
+        model = BlogImage
+        fields = ('image',)
 
 
 class CommentForm(forms.ModelForm):
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib.auth.decorators import login_required
 
 from .models import BlogPost, BlogImage, BlogComment
-from .forms import CommentForm
+from .forms import BlogForm, BlogImageForm, CommentForm
 
 
 def blog_feed(request):
@@ -117,3 +117,114 @@ def delete_comment(request, comment_pk):
 
     messages.success(request, 'Successfully deleted your comment.')
     return redirect(post_view, slug)
+
+
+@login_required
+def add_blog(request):
+    """ Add a product to the store """
+    if not request.user.is_superuser:
+        messages.error(request,
+                       'Sorry, only store owners have access to the area.')
+        return redirect(reverse('home'))
+
+    if request.method == 'POST':
+        blog_form = BlogForm(request.POST)
+        blog_image_form = BlogImageForm(request.POST, request.FILES)
+        if blog_form.is_valid() and blog_image_form.is_valid():
+            # Create Blog object but don't save to database yet
+            new_blog = blog_form.save(commit=False)
+            new_blog_image = blog_image_form.save(commit=False)
+            # Assign the auther to the new blog and save it
+            new_blog.author = request.user
+            new_blog.save()
+            # Assign the blog to the new image and save it
+            new_blog_image.article_id_id = new_blog.pk
+            new_blog_image.save()
+            messages.success(request, 'Successfully posted your blog.')
+        else:
+            messages.error(
+                request, 'Failed to add the blog. Please ensure the form is valid.')
+
+    else:
+        blog_form = BlogForm()
+        blog_image_form = BlogImageForm()
+
+    if 'last_item' in request.session:
+        del request.session['last_item']
+
+    template = 'blog/add_blog.html'
+    context = {
+        'blog_form': blog_form,
+        'blog_image_form': blog_image_form,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def edit_blog(request, blog_pk):
+    """Edit a product in the store"""
+    if not request.user.is_superuser:
+        messages.error(request,
+                       'Sorry, only site owner have access to the area.')
+        return redirect(reverse('home'))
+
+    blog_post = get_object_or_404(BlogPost, pk=blog_pk)
+    blog_image = get_object_or_404(BlogImage, article_id=blog_pk)
+
+    if request.method == 'POST':
+        blog_form = BlogForm(request.POST, instance=blog_post)
+        blog_image_form = BlogImageForm(request.POST, request.FILES, instance=blog_image)
+
+        if blog_form.is_valid() and blog_image_form.is_valid():
+            # Create Blog object but don't save to database yet
+            new_blog = blog_form.save(commit=False)
+            new_blog_image = blog_image_form.save(commit=False)
+            # Assign the auther to the new blog and save it
+            new_blog.author = request.user
+            new_blog.save()
+            # Assign the blog to the new image and save it
+            new_blog_image.article_id_id = new_blog.pk
+            new_blog_image.save()
+            messages.success(request, f'You updated {blog_post.title}!')
+
+            if 'last_item' in request.session:
+                del request.session['last_item']
+
+            return redirect(reverse('post_view', args=[blog_post.slug]))
+        else:
+            messages.error(
+                request, 'Failed to update blog. Please ensure the form is valid.')
+
+    else:
+        blog_form = BlogForm(instance=blog_post)
+        blog_image_form = BlogImageForm(request.POST, request.FILES, instance=blog_image)
+        messages.info(
+            request, f'You are editing a product details: {blog_post.title}')
+
+    template = 'blog/edit_blog.html'
+    context = {
+        'blog_form': blog_form,
+        'blog_image_form': blog_image_form,
+        'blog_post': blog_post,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def delete_blog(request, blog_pk):
+    if not request.user.is_superuser:
+        messages.error(request,
+                       'Sorry, only site owner have access to the area.')
+        return redirect(reverse('home'))
+
+    blog = get_object_or_404(BlogPost, pk=blog_pk)
+    blog.delete()
+    messages.success(request,
+                     f'You deleted blog title: {blog.title} from the database.')
+
+    if 'last_item' in request.session:
+        del request.session['last_item']
+
+    return redirect(reverse('blog'))
