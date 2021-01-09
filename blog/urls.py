ae018e1ff4484189b14248177ea831d0668ec98f
Asuna Masuda <asuna.fitz@gmail.com>
2021-01-09 12:34:12 +0000

    Add path to add, adit, delete blog

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('blog_detail/<slug:slug>/', views.post_view, name='post_view'),
     path('delete_comment/<int:comment_pk>', views.delete_comment,
          name='delete_comment'),
+    path('add/', views.add_blog, name='add_blog'),
+    path('edit/<int:blog_pk>/', views.edit_blog, name='edit_blog'),
+    path('delete/<int:blog_pk>/', views.delete_blog, name='delete_blog'),
 ]
