8a52710738df1feb734247c1ac189bfc59ff6004
Asuna Masuda <asuna.fitz@gmail.com>
2021-01-10 22:03:28 +0000

    Change DEBUG to  'DEVELOPMENT' in os.environ

--- flowery_days/settings.py
+++ flowery_days/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['flowerydays.herokuapp.com', 'localhost', '127.0.0.1']
 
