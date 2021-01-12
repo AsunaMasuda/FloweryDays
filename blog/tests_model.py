faa538e2d2d9165f973884a07d43d0dced76f3ae
Asuna Masuda <asuna.fitz@gmail.com>
2021-01-12 00:59:23 +0000

    Remove test for blog comments

--- blog/tests_model.py
+++ blog/tests_model.py
@@ -23,21 +23,3 @@ class TestBlogImageModel(TestCase):
                                               content_type='image/jpeg')
         self.assertEqual(str(blog_image),
                          'test_image.jpg, blog_title, category')
-
-
-class TestBlogCommentModel(TestCase):
-
-    def test_blog_comment(self):
-        user_1 = User(username='test_user_1')
-        user_1.save()
-        blog = BlogPost(title='blog_title', category='category', author=user_1)
-        blog.save()
-        user_2 = User(username='test_user_2')
-        user_2.save()
-        blog_comment = BlogComment(article_id=blog,
-                                   user_id=user_2,
-                                   comment_title='test_comment_title')
-
-        self.assertEqual(str(blog_comment),
-                         'blog_title, category, test_user_2, \
-                         test_comment_title')
