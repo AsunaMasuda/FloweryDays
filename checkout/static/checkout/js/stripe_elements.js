df709a79e82cc27238077d00f642234a0e1a1926
Asuna Masuda <asuna.fitz@gmail.com>
2021-01-12 21:54:32 +0000

    Validate/Format js files

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -17,11 +17,13 @@ var style = {
         iconColor: '#dc3545'
     }
 };
-var card = elements.create('card', {style: style});
+var card = elements.create('card', {
+    style: style
+});
 card.mount('#card-element');
 
 // Handle realtime validation errors on the card element
-card.addEventListener('change', function (event) {
+card.addEventListener('change', function(event) {
     var errorDiv = document.getElementById('card-errors');
     if (event.error) {
         var html = `
@@ -41,36 +43,38 @@ var form = document.getElementById('payment-form');
 
 form.addEventListener('submit', function(ev) {
     ev.preventDefault();
-    card.update({ 'disabled': true });
+    card.update({
+        'disabled': true
+    });
     $('#submit-button').attr('disabled', true);
-    $('#payment-form').fadeToggle(100)
-    $('#loading-overlay').fadeToggle(100)
+    $('#payment-form').fadeToggle(100);
+    $('#loading-overlay').fadeToggle(100);
 
-    var saveInfo = Boolean($('#id-save-info').attr('checked'))
+    var saveInfo = Boolean($('#id-save-info').attr('checked'));
     var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
     var postData = {
         'csrfmiddlewaretoken': csrfToken,
         'client_secret': clientSecret,
         'save_info': saveInfo,
     };
-    var url = '/checkout/cache_checkout_data/'
+    var url = '/checkout/cache_checkout_data/';
 
-    $.post(url, postData).done(function(){
+    $.post(url, postData).done(function() {
         stripe.confirmCardPayment(clientSecret, {
-        payment_method: {
-            card: card,
-            billing_details: {
-                name: $.trim(form.first_name.value).concat(' ', $.trim(form.last_name.value)),
-                email: $.trim(form.email.value),
-                address:{
-                    line1: $.trim(form.address_line_1.value),
-                    line2: $.trim(form.address_line_2.value),
-                    city: $.trim(form.town_or_city.value),
-                    state: $.trim(form.county.value),
+            payment_method: {
+                card: card,
+                billing_details: {
+                    name: $.trim(form.first_name.value).concat(' ', $.trim(form.last_name.value)),
+                    email: $.trim(form.email.value),
+                    address: {
+                        line1: $.trim(form.address_line_1.value),
+                        line2: $.trim(form.address_line_2.value),
+                        city: $.trim(form.town_or_city.value),
+                        state: $.trim(form.county.value),
+                    }
                 }
-            }
-        },
-        shipping: {
+            },
+            shipping: {
                 name: $.trim(form.first_name.value).concat(' ', $.trim(form.last_name.value)),
                 address: {
                     line1: $.trim(form.address_line_1.value),
@@ -80,27 +84,29 @@ form.addEventListener('submit', function(ev) {
                     postal_code: $.trim(form.postcode.value),
                 }
             },
-    }).then(function(result) {
-        if (result.error) {
-            var errorDiv = document.getElementById('card-errors');
-            var html = `
+        }).then(function(result) {
+            if (result.error) {
+                var errorDiv = document.getElementById('card-errors');
+                var html = `
                 <span class="icon" role="alert">
                 <i class="fas fa-times"></i>
                 </span>
                 <span>${result.error.message}</span>`;
-            $(errorDiv).html(html);
-            $('#payment-form').fadeToggle(100)
-            $('#loading-overlay').fadeToggle(100)
-            card.update({ 'disabled': false});
-            $('#submit-button').attr('disabled', false);
-        } else {
-            if (result.paymentIntent.status === 'succeeded') {
-                form.submit();
+                $(errorDiv).html(html);
+                $('#payment-form').fadeToggle(100);
+                $('#loading-overlay').fadeToggle(100);
+                card.update({
+                    'disabled': false
+                });
+                $('#submit-button').attr('disabled', false);
+            } else {
+                if (result.paymentIntent.status === 'succeeded') {
+                    form.submit();
+                }
             }
-        }
-    });
-    }).fail(function(){
+        });
+    }).fail(function() {
         // just reload the page, the error will be in django message
         location.reload();
-    })
+    });
 });
--- static/js/base.js
+++ static/js/base.js
@@ -1,81 +1,79 @@
 // Search bar for less than medium screens
-$(document).ready(function () {
-  $('#search-bar-small').hide();
-  $('#search-button').click(function () {
-    $('#search-bar-small').slideToggle('slow');
-  });
+$(document).ready(function() {
+    $('#search-bar-small').hide();
+    $('#search-button').click(function() {
+        $('#search-bar-small').slideToggle('slow');
+    });
 });
 
 // Wow.js
-$(document).ready(function () {
+$(document).ready(function() {
     new WOW().init();
 });
 
-$(window).scroll(function () {
-  if ($(window).scrollTop() <= 100) {
-    $("#logo").css("display", "block");
-  } else {
-    $("#logo").css("display", "none");
-  }
+$(window).scroll(function() {
+    if ($(window).scrollTop() <= 100) {
+        $("#logo").css("display", "block");
+    } else {
+        $("#logo").css("display", "none");
+    }
 });
 
-$(window).scroll(function () {
-  if ($(window).scrollTop() > 100) {
-    $("#logo-small").css("display", "block");
-  } else {
-    $("#logo-small").css("display", "none");
-  }
+$(window).scroll(function() {
+    if ($(window).scrollTop() > 100) {
+        $("#logo-small").css("display", "block");
+    } else {
+        $("#logo-small").css("display", "none");
+    }
 });
 
 // Quantity Counter - Cart View
-$(document).ready(function(){
-    $('.plus').click(function(){
+$(document).ready(function() {
+    $('.plus').click(function() {
         $(this).siblings('.qty_input').val(parseInt($(this).siblings('.qty_input').val()) + 1);
-            if ($(this).siblings('.qty_input').val() == 100) {
-                $(this).siblings('.qty_input').val(99);
-            }
-        });
-    $('.minus').click(function(){
+        if ($(this).siblings('.qty_input').val() == 100) {
+            $(this).siblings('.qty_input').val(99);
+        }
+    });
+    $('.minus').click(function() {
         $(this).siblings('.qty_input').val(parseInt($(this).siblings('.qty_input').val()) - 1);
-            if ($(this).siblings('.qty_input').val() == 0) {
-                $(this).siblings('.qty_input').val(1);
-            }
+        if ($(this).siblings('.qty_input').val() == 0) {
+            $(this).siblings('.qty_input').val(1);
+        }
     });
     $(document).on('change', '.quantity-count', function() {
         $(this).val($(this).val());
-            if (($(this).val() > 99)) {
-                $('.errorMsg').show();
-                $(this).val(99);
-            }
-            else if (($(this).val() < 1 )) {
-                $('.errorMsg').show();
-                $(this).val(1);
-            }
-            else{
-                $('.errorMsg').hide();
-            }
+        if (($(this).val() > 99)) {
+            $('.errorMsg').show();
+            $(this).val(99);
+        } else if (($(this).val() < 1)) {
+            $('.errorMsg').show();
+            $(this).val(1);
+        } else {
+            $('.errorMsg').hide();
+        }
     });
 });
 
 // Back to previous page
 $(document).ready(function() {
-    $(document).on('click', '.backToPrevious', function(){
+    $(document).on('click', '.backToPrevious', function() {
         window.history.back();
     });
 });
 
 // Customer Review
-$(document).ready(function () {
+$(document).ready(function() {
     $('.review_click_arrow_1').click(function() {
         $(this).find('i').toggleClass('fa-chevron-up fa-chevron-down');
         if ($('.review_click_arrow_2').find('i').hasClass('fa-chevron-up')) {
-        $('.review_click_arrow_2').find('i').toggleClass('fa-chevron-up fa-chevron-down');
+            $('.review_click_arrow_2').find('i').toggleClass('fa-chevron-up fa-chevron-down');
         }
     });
     $('.review_click_arrow_2').click(function() {
         $(this).find('i').toggleClass('fa-chevron-up fa-chevron-down');
         if ($('.review_click_arrow_1').find('i').hasClass('fa-chevron-up')) {
-        $('.review_click_arrow_1').find('i').toggleClass('fa-chevron-up fa-chevron-down');
+            $('.review_click_arrow_1').find('i').toggleClass('fa-chevron-up fa-chevron-down');
         }
     });
 });
