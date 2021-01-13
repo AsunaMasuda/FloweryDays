1ce738016d6807d49e91c7482774e0eea853f082
Asuna Masuda <asuna.fitz@gmail.com>
2021-01-13 00:48:14 +0000

    Add django.template.context_processors.media

--- flowery_days/settings.py
+++ flowery_days/settings.py
@@ -84,6 +84,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'cart.contexts.cart_contents',
             ],
             'builtins': [
